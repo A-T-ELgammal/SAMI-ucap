d373dc4e4718d49971cf7d85c7ccc0bb6f878ef4
atelgammal <a.t.elgammal@gmail.com>
2024-08-21 09:51:28 +0300

    implement script into program contribution

--- com.TulipTechnologies.SAMIRobot/src/main/java/com/TulipTechnologies/SAMIRobot/impl/PainterRobotProgramNodeContribution.java
+++ com.TulipTechnologies.SAMIRobot/src/main/java/com/TulipTechnologies/SAMIRobot/impl/PainterRobotProgramNodeContribution.java
@@ -1,5 +1,10 @@
 package com.TulipTechnologies.SAMIRobot.impl;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import com.ur.urcap.api.contribution.ProgramNodeContribution;
 import com.ur.urcap.api.domain.script.ScriptWriter;
 
@@ -31,8 +36,24 @@ public class PainterRobotProgramNodeContribution implements ProgramNodeContribut
 
     @Override
     public void generateScript(ScriptWriter writer) {
-        
-
+        InputStream is = getClass().getResourceAsStream("/painter_robot.urscript");
+        try{        
+        if (is != null)
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while((line = br.readLine()) != null)
+            {
+                writer.appendLine(line);
+            }
+            br.close();
+        }else{}
+            writer.appendLine("textmsg(\"Error: socketConnection.urscript file not found\")");
+        }
+        catch(IOException e)
+        {
+            writer.appendLine("textmsg(\"Error reading socketConnection.urscript file: " + e.getMessage() + "\")");
+        }
     }
     
 }
